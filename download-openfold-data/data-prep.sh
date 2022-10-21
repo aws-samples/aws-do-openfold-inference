b4c0f264d882137eb5632e0804508ea99ac2eee2
Ankur Srivastava <awsankur@amazon.com>
2022-10-21 13:00:58 -0700

    Updated data-prep.sh

--- download-openfold-data/data-prep.sh
+++ download-openfold-data/data-prep.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# NOTE: THIS TAKES WAY TOO LONG. THERE SHOULD BE A BETTER SOLUTION
+# NOTE: THIS TAKES ABOUT 4 HOURS
 
 
 echo "download data ......."
 
+bash scripts/download_alphafold_params_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_mgnify_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_pdb_mmcif_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_small_bfd_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_uniprot_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_bfd_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_pdb70_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_pdb_seqres_s3.sh /fsx-shared/openfold/data/
+bash scripts/download_uniclust30_s3.sh /fsx-shared/openfold/data/
 bash scripts/download_uniref90_s3.sh /fsx-shared/openfold/data/
 
 echo "***DONE***"
